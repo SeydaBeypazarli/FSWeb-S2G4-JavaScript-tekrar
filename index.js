6e00b9595a830b7e6e6519d61bcf485b82373af5
SeydaBeypazarli <seydabeypazarli@gmail.com>
2024-09-07 12:15:44 +0300

    görev 1 tamamlandı

--- index.js
+++ index.js
@@ -50,16 +50,15 @@ function KareninAlani(kenaruzunlugu) {
 	4. Hesaplanan çemberin çevresi döndürülecektir.
 */
 
+/* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
 function CemberinCevresi(yaricap) {
   // Math.PI sabitini kullanarak çemberin çevresini hesapla
   const cevre = 2 * Math.PI * yaricap;
   // Çevreyi döndür
   return cevre;
 }
-
 // Örnek kullanım
 console.log(CemberinCevresi(5));
-/* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
 
 /* 	GÖREV 2:  
 - CemberinAlani fonksiyonunu kullanarak aşağıdaki yönergeleri uygulayın:
